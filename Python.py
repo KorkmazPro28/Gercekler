56711caf8dd4230a50da1d899e4e6afe5d6b319a
KorkmazPro28 <123293392+KorkmazPro28@users.noreply.github.com>
2023-04-28 22:19:49 +0300

    Python
    Python

---
+++ Python.py
@@ -0,0 +1,4 @@
+import random 
+
+gercekler = ["En Sevdiğim Hobim : Elektronik Yazılım ","Sitemle Çok Gurur Duyuyorum","En Sevdiğim Yemek : Salata","Rastgele Bir Bilgi Gösteren Modül : Random","En Sevdiğin Gezegen : Jüpiter"]
+print(random.choice(gercekler))
